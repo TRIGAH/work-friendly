c7fb6f677ad2d911075dae4360f903ee931e067d
Trigah <ijenrandy2@gmail.com>
2024-02-10 02:10:02 +0100

    Add Add Cafe route

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from flask_wtf import FlaskForm
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask import jsonify
+from wtforms import StringField, SubmitField,BooleanField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -29,13 +31,35 @@ class Cafe(db.Model):
     coffee_price = db.Column(db.String(250), nullable=True)
 
 
+class CafeForm(FlaskForm):
+    cafe = StringField('Cafe name', validators=[DataRequired()])
+    location = StringField('Cafe Location on Gooogle Maps (URL)', validators=[DataRequired()])
+    coffee = BooleanField('Has Coffee ', validators=[DataRequired()],default=False)
+    wifi = BooleanField('Has WIFI ', validators=[DataRequired()],default=False)
+    power = BooleanField('has Power Socket ', validators=[DataRequired()],default=False)
+    submit = SubmitField('Submit')
+
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
+@app.route('/add',methods=['GET','POST'])
+def add_cafe():
+    form = CafeForm()
+    if form.validate_on_submit():
+        new_cafe = Cafe(
+        name=form.cafe.data,
+        map_url=form.location.data)
+        db.session.add(new_cafe)
+        db.session.commit()
+
+    return render_template('add.html', form=form)
+
+
 @app.route('/cafes')
 def cafes():
-    cafes_list=[]
     all_cafes = db.session.query(Cafe).all()
     # for cafe in all_cafes:
     #     cafes_list.append({"id":cafe.id,
